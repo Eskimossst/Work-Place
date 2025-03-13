138dc90a10c9b84c3dcd4689e7ec2c66b964f365
Eskimos <49443159+Eskimossst@users.noreply.github.com>
2025-03-13 16:06:46 +0300

    Third

---
+++ app.js
@@ -0,0 +1,52 @@
+let file = 100;
+let firstName = 'Reinar'
+const isProgrammer = true
+
+firstName = "Henrikki"
+// isProgrammer = false
+
+//console.log('Test',firstName)
+//console.log(file + 10)
+
+//let file2 = file +25
+//console.log(file, file2)
+const fullName = firstName + ' Jarvinen'
+console.log(fullName)
+
+const resultElement = document.getElementById("sum")
+// console.log(resultElement.textContent)
+// resultElement.textContent = 100
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const copyResult = document.getElementById('calcu')
+// console.log(input1.value)
+// console.log(input2.value)
+
+const sume = Number(input1.value) + Number(input2.value)
+console.log(typeof sume)
+
+copyResult.onclick = function () {
+    console.log('Hello Click!')
+}
+
+// let name = "Henrikki"
+// let age = " 29.11.1999"
+// let words = " fly"
+// let agree = name + age + words
+
+// console.log (agree)
+
+const array = [1, 2, 3]
+
+console.log (array)
+
+const inputElement = document.getElementById ('names')
+const inputElementAge = document.getElementById ('age')
+const createBtn = document.getElementById ('agree')
+const listElement = document.getElementById ('list-some')
+
+// console.log (inputElement.value)
+
+createBtn.onclick = function () {
+    listElement.innerHTML = inputElement.value 
+}
